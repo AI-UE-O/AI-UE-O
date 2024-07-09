09451009c564ea502c8e2246f520a9461a1a7352
ShohTagawa <tagawa.s@faculty.gs.chiba-u.jp>
2024-07-09 17:29:02 +0900

    test

---
+++ content/posts/my-first-post.md
@@ -0,0 +1,5 @@
++++
+title = 'My First Post'
+date = 2024-07-09T17:16:39+09:00
+draft = true
++++
---
+++ public/js/preferred-lang.js
@@ -0,0 +1,1 @@
+(()=>{var o={fallback:"https://ai-ue-o.jp/",homes:{en:"https://ai-ue-o.jp/"}};(()=>{let a=navigator.language||navigator.userLanguage;if(a in o.homes){window.location.href=o.homes[a];return}let n=a.split("-");for(let e in o.homes)if(e.indexOf(n[0])===0){window.location.href=o.homes[e];return}window.location.href=o.fallback})();})();
